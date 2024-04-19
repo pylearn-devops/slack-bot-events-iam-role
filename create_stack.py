aad950133d747603c08802012a9261f1220942bf
Akash Basa <39308879+aakashbasa@users.noreply.github.com>
2024-04-19 17:47:17 -0400

    Update create_stack.py

--- create_stack.py
+++ create_stack.py
@@ -4,14 +4,10 @@ import boto3
 from tabulate import tabulate
 
 
-boto3_client = boto3.client(
-    'cloudformation',
+client = boto3.client('cloudformation',
     region_name='us-east-1',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-)
-
-client = boto3.client(boto3_client)
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
 
 # Define your stack name and S3 template URL
 stack_name = 'ab-iam-role-stack'
